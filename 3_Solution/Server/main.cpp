2caeab65cd220a2052a73cb80e33710c825af244
RoscaGeorgeMihai <162510735+RoscaGeorgeMihai@users.noreply.github.com>
2024-03-27 13:38:18 +0200

    Conexiune Baza de date - TEST
    Signed-off-by: RoscaGeorgeMihai <162510735+RoscaGeorgeMihai@users.noreply.github.com>

--- 3_Solution/Server/main.cpp
+++ 3_Solution/Server/main.cpp
@@ -1,12 +1,13 @@
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
 #include "APPServer.h"
+#include "BDCom.h"
 
 int main() {
 	
 	// blocks untill someone connects
+	BDCom newObj;
 	APPServer::getInstance()->getTcpServer()->wait_connection();
-
 	char buffer[1024];
 	for (;;) {
 		int recv_bytes = APPServer::getInstance()->getTcpServer()->recv(buffer, 1024);
